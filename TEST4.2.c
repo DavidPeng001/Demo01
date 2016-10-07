278b42b428ec7f39dd6414a13c2a5b21ee1280e5
DavidP001 <pp876314955@hotmail.com>
2016-10-07 22:00:03 +0800

    TEST

---
+++ TEST4.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	
+	char fname[40];
+	char lname[40];
+	
+	printf("please input your first name and last name :\n");
+	scanf("%s %s",fname,lname);
+	printf("\"%s %s\"\n",fname,lname);
+	printf("");
+	
+} 
---
+++ TEST4.5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define CHANGE 8
+
+int main(){
+	
+	double speed = 18.12;  // Mb/s
+	double bytes = 2.20;//  MB
+	double time;
+	double bits;
+	
+	bits = bytes * CHANGE;
+	time = bits / speed;
+	
+	printf("At %.2f megabits per second , a file of %.2f megabytes \n",speed,bytes);
+	printf("downloads in %.2f seconds.");
+	
+	return 0;
+
+}
